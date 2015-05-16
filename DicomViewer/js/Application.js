a19e51c249f47d1e1a66e6ab135ee56992d8480d
Dominik <dominiklochynski@gmail.com>
2015-05-16 22:03:44 +0200

    wstepny zoom

--- DicomViewer/js/Application.js
+++ DicomViewer/js/Application.js
@@ -1,11 +1,14 @@
-﻿function fillCanvas(canvas, url) {
+﻿function fillCanvas(canvas, url, scale) {
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.canvas.width = $('.main-content').width();
     ctx.canvas.height = window.innerHeight;
+    if(scale)ctx.scale(scale, scale);
     var imageObj = new Image();
     imageObj.onload = function () {
-        ctx.drawImage(imageObj, 0, 0);
+        var width = imageObj.naturalWidth;
+        var height = imageObj.naturalHeight;
+        ctx.drawImage(imageObj, 0, 0, width, height);
     };
     imageObj.src = '/Dicom/GetImage/' + url;
 }
@@ -20,24 +23,53 @@ function loadMainContent(reqUrl) {
     });
 }
 function reFillCanvasAfterAjax(url) {
-    $(document).ajaxComplete(function () {
         canvas = document.getElementById("canvas");
         fillCanvas(canvas, url);
+        return canvas;
+}
+
+function changeZoom(canvas, url, zoom) {
+    fillCanvas(canvas, url, zoom);
+    
+}
+
+function enableZoom(canvas, url) {
+    var zoom = 1;
+    $('input.zoom-change').on('click', function () {
+        if ($(this).hasClass('zoom-in')) {
+            zoom++;
+        } else {
+           if(zoom > 1) zoom--;
+        }
+        if (canvas && url) {
+            changeZoom(canvas, url, zoom);
+        }
     });
 }
 
 
 $(document).ready(function () {
-    
+    var canvas, url, zoom = 1;
+
     $('.table.left-menu-table .img-name').on('click', function () {
-       var url = $(this).attr('id')
-        var canvas = document.getElementById("canvas");
-        if (canvas) fillCanvas(canvas, url);
+        url = $(this).attr('id')
+        canvas = document.getElementById("canvas");
+        if (canvas) {
+            fillCanvas(canvas, url);
+            enableZoom(canvas, url);
+        }
         else {
             loadMainContent(url);
-            reFillCanvasAfterAjax(url);
+            $(document).ajaxComplete(function () {
+                canvas = reFillCanvasAfterAjax(url);
+                enableZoom(canvas, url);
+            });
         }
     });
+   
+   
+
+    
 
    
 
